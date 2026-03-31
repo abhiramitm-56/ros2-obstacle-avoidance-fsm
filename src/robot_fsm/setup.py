13aa878da5718aa1aac6231921a94ec8f03cafb9
abhiramitm55-glitch <your-github-abhiramitm55@gamil.com>
2026-03-31 22:30:24 +0530

    Initial commit: FSM obstacle avoidance

---
+++ src/robot_fsm/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'robot_fsm'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='abhirami',
+    maintainer_email='your-github-abhiramitm55@gamil.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        'obstacle_avoidance = robot_fsm.obstacle_avoidance:main',
+        ],
+    },
+)
